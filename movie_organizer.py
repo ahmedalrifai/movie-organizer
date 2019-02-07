30bcd30a110ecb5e99a4abbc2366bc91eeb16b05
Ahmed Alrifai <ahmed.alrifai.97@gmail.com>
2019-02-07 05:58:09 +0200

    Add function to move movie folder to it's rating folder category

--- movie_organizer.py
+++ movie_organizer.py
@@ -1,4 +1,31 @@
+import os
+import shutil
+
+import omdb
+
+
+omdb.set_default('apikey', '63bd66b7')
+
+
 def title_formater(title):
     if '(' in title:
         title = title[:title.index(' (')]
     return title
+
+
+def organize_movies(path):
+    movies = os.listdir(path)
+    os.chdir(path)
+    for title in movies:
+        if title in [str(i) for i in range(1, 11)]:
+            continue
+        formated_title = title_formater(title)
+        rating = omdb.title(formated_title)['imdb_rating']
+        print(f'Moving "{formated_title}" to folder {rating[0]}')
+        if not os.path.exists(rating[0]):
+            os.mkdir(rating[0])
+        shutil.move(title, rating[0])
+
+
+if '__main__' == __name__:
+    organize_movies('')
